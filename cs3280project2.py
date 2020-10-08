1bae563613fe505fd0bddf7e3b6a116d82eb83e9
exerlin <ejensen1@my.westga.edu>
2020-10-08 18:15:09 -0400

    Moved IP and Subnet verification to utils.py. Refined those methods to work as intended.

--- cs3280project2.py
+++ cs3280project2.py
@@ -4,6 +4,7 @@ Main script that accepts user input and provides methods.
 '''
 
 import re
+import utils
 
 def regex_ip_address_check_for_errors(the_address):
     '''
@@ -17,36 +18,14 @@ def regex_ip_address_check_for_errors(the_address):
         return True
     return False
 
-def regex_subnet_mask_check_for_errors(the_subnet_mask):
-    '''
-    regex_subnet_mask_check_for_errors(the_subnet_mask) that returns a boolean
-    Returns True if the given subnet mask is in the format of a subnet mask.
-    Returns False otherwise.
-    '''
-    regex_string_subnet_mask_address_format = re.compile(r'^[0-9]{3}([0-9]{1,3}){3}$')
-    regex_string_subnet_mask_bit_format = re.compile(r'^[0-2]?[0-9]$|^3[0,1]$')
-
-    if regex_string_subnet_mask_address_format.search(the_subnet_mask) is not None:
-        return True
-    if regex_string_subnet_mask_bit_format.search(the_subnet_mask) is not None:
-        return True
-    return False
-
 def test_method():
-    print(regex_ip_address_check_for_errors('192.168.0.0'))
-    print(regex_ip_address_check_for_errors('192.16.011.0'))
-    print(regex_ip_address_check_for_errors('192.168.0.0.'))
+    print(utils.verify_ip_address_format('192.168.0.0'))
+    print(utils.verify_ip_address_format('192.16.011.0'))
+    print(utils.verify_ip_address_format('192.168.0.0.'))
 
-    print(regex_subnet_mask_check_for_errors('0'))
-    print(regex_subnet_mask_check_for_errors('01'))
-    print(regex_subnet_mask_check_for_errors('1'))
-    print(regex_subnet_mask_check_for_errors('20'))
-    print(regex_subnet_mask_check_for_errors('29'))
-    print(regex_subnet_mask_check_for_errors('30'))
-    print(regex_subnet_mask_check_for_errors('31'))
-    print(regex_subnet_mask_check_for_errors('32'))
-    print(regex_subnet_mask_check_for_errors('40'))
-    print(regex_subnet_mask_check_for_errors('a'))
+    print(utils.verify_subnet_mask_format('255.254.0.0'))
+    print(utils.verify_subnet_mask_format('255.255.128.0'))
+    print(utils.verify_subnet_mask_format('255.255.0.255'))
 
 def main():
     '''
---
+++ utils.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+'''
+Script that contains code to verify the validity of IP and subnet addresses.
+Can also get the subnet given an IP address.
+'''
+
+import re
+
+def verify_ip_address_format(the_address):
+    '''
+    verify_ip_address_format(the_address) that returns a boolean
+    Returns True if the given address is in the format of an ip address.
+    Returns False otherwise.
+    '''
+    regex_string_ip_address = re.compile(r'^[0-9]{3}[.]{1}([0-9]{1,3}[.]{1}){2}[0-9]{1,3}$')
+
+    if regex_string_ip_address.search(the_address) is not None:
+        return True
+    return False
+
+def verify_subnet_mask_format(the_subnet_mask):
+    '''
+    verify_subnet_mask_format(the_subnet_mask) that returns a boolean
+    Returns True if the given subnet mask is in the format of a subnet mask.
+    Returns False otherwise.
+    '''
+    regex_string_subnet_mask_address_format = re.compile(r'^(((128|192|224|240|248|252|254|255|0(?=\.0))\.){3}(128|192|224|240|248|252|254|255|0))$')
+    if regex_string_subnet_mask_address_format.search(the_subnet_mask) is not None:
+        return True
+    return False
+
+def get_full_subnet_mask(number_of_bits):
+    '''
+    Returns a formatted subnet mask given a number of bits.
+    Returns False if number of bits given is invalid.
+    '''
+    if number_of_bits > 31 or number_of_bits < 0:
+        return False
+    full_subnet_mask = ""
+    for current_division in range(4):
+        if not full_subnet_mask == "":
+            full_subnet_mask += "."
+            
+        if number_of_bits > 7:
+            full_subnet_mask += "255"
+            number_of_bits -= 8
+        elif number_of_bits == 7:
+            full_subnet_mask += "254"
+            number_of_bits -= 7
+        elif number_of_bits == 6:
+            full_subnet_mask += "252"
+            number_of_bits -= 6
+        elif number_of_bits == 5:
+            full_subnet_mask += "248"
+            number_of_bits -= 5
+        elif number_of_bits == 4:
+            full_subnet_mask += "240"
+            number_of_bits -= 4
+        elif number_of_bits == 3:
+            full_subnet_mask += "224"
+            number_of_bits -= 3
+        elif number_of_bits == 2:
+            full_subnet_mask += "192"
+            number_of_bits -= 2
+        elif number_of_bits == 1:
+            full_subnet_mask += "128"
+            number_of_bits -= 1
+        elif number_of_bits == 0:
+            full_subnet_mask += "0"
+            number_of_bits -= 0
+    return full_subnet_mask
+
+
+
+
+
+
+
+    
